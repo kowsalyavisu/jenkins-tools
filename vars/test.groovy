6eb51387865233cbfb87ee5fac4caf228f8348be
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-26 21:50:52 -0600

    test commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -24,7 +24,7 @@ def call() {
               "curl -s",
               "-XPOST 'https://api.lytics.io/api/query/_test?email=test@test.com&ucdmid=ucdmid'",
               "-H 'Content-type: application/json'",
-              "-H 'Authorization: at.0324245c93ba580383bf52a5e8c50ff8.ebbe2a01aa043fc7f13a864255fc25da'",
+              "-H 'Authorization: at.0324245c93ba580383bf52a5e8c50ff8.ebbe2a01aa043fc7f13a864255fc25d'",
               "--data-binary @user_redshift.lql"
               
           ]
