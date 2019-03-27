d12cd5138b54265eb505a6efdebdf621feb67704
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-26 22:02:57 -0600

    test commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -22,9 +22,9 @@ def call() {
         stage('test-lytics') {
           def curlCommand = [
               "curl --show-error --fail",
-              "-XPOST 'https://api.lytics.io/api/query/_test?email=test@test.com&ucdmid=ucdmid'",
+              "-XPOST 'https://api.lytics.io/api/query/_test?emai=test@test.com&ucdmi=ucdmid'",
               "-H 'Content-type: application/json'",
-              "-H 'Authorization: at.0324245c93ba580383bf52a5e8c50ff8.ebbe2a01aa043fc7f13a864255fc25d'",
+              "-H 'Authorization: at.0324245c93ba580383bf52a5e8c50ff8.ebbe2a01aa043fc7f13a864255fc25da'",
               "--data-binary @user_redshift.lql"
               
           ]
