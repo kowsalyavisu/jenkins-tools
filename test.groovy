4aae0d159887d06046febe4295b56c66a954b2e0
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-26 19:22:08 -0600

    testing

---
+++ test.groovy
@@ -0,0 +1,18 @@
+def call() {
+  node {
+    checkout changelog: false,
+      poll: false,
+      scm: [
+          $class: 'GitSCM',
+          branches: 'master',
+          doGenerateSubmoduleConfigurations: false,
+          submoduleCfg: [],
+          userRemoteConfigs: [[
+                                  credentialsId: 'kowsalyavisu@gmail.com',
+                                  name: 'origin',
+                                  url: 'https://github.com/kowsalyavisu/lql.git'
+                              ]]
+      ]
+
+    }
+}
