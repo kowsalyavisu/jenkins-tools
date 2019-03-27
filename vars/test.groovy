29f9292bf852c780d4091ccf1303455fa2229927
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-26 19:27:46 -0600

    test file

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,15 +1,19 @@
 def call() {
-  checkout changelog: false,
-      poll: false,
-      scm: [
-          $class: 'GitSCM',
-          branches: 'master',
-          doGenerateSubmoduleConfigurations: false,
-          submoduleCfg: [],
-          userRemoteConfigs: [[
-                                  credentialsId: 'kowsalyavisu@gmail.com',
-                                  name: 'origin',
-                                  url: 'https://github.com/kowsalyavisu/lql.git'
-                              ]]
-      ]
+  node {
+    stage('prep') {
+      checkout changelog: false,
+          poll: false,
+          scm: [
+              $class: 'GitSCM',
+              branches: 'master',
+              doGenerateSubmoduleConfigurations: false,
+              submoduleCfg: [],
+              userRemoteConfigs: [[
+                                      credentialsId: 'kowsalyavisu@gmail.com',
+                                      name: 'origin',
+                                      url: 'https://github.com/kowsalyavisu/lql.git'
+                                  ]]
+          ]
+        }
+      }
 }
