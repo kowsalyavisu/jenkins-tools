08ea2466a27e645c2913786c06bc95c0a77d05dc
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-28 15:31:35 -0600

    add map

--- vars/lqlValidate.groovy
+++ vars/lqlValidate.groovy
@@ -2,21 +2,14 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 def call(String token, String testParam, String fileName, String env) {
 
-    //def fileContents = readFile fileName+".json"
-    //print(fileContents)
-    //final slurper = new JsonSlurperClassic()
-    //Map queryParam = new HashMap<>(slurper.parseText(fileContents))
-    //print(queryParam)
     print("before calling method")
-    def map = getUserData(fileName, env)
-    print(map)
+    def queryParam = getUserData(fileName, env)  
     def curlCommand = [
         "curl --show-error --fail",
-        "-XPOST 'https://api.lytics.io/api/query/_test?"+map+"'",
+        "-XPOST 'https://api.lytics.io/api/query/_test?"+queryParam+"'",
         "-H 'Content-type: application/json'",
         "-H 'Authorization: "+token+"'",
-        "--data-binary @"+fileName+".lql"
-        
+        "--data-binary @"+fileName+".lql" 
     ]
     sh curlCommand.join(" ")
 }
@@ -25,12 +18,8 @@ def call(String token, String testParam, String fileName, String env) {
 
 @NonCPS
 def getUserData(String fileName, String env) {
-  print(env)
-   String fileContent = new File("/Users/kviswanathan/.jenkins/workspace/test-pipe/user_redshift.json").text
-   def jsonSlurper = new JsonSlurper() 
-   def resultJson = jsonSlurper.parseText(fileContent)
-   print("result-->"+resultJson)
-   def res = resultJson.collect { it }.join('&')
-   print(res)
-   return res
+  def fileContent = new File("/Users/kviswanathan/.jenkins/workspace/test-pipe/user_redshift.json").text
+  def resultJson = new JsonSlurper() .parseText(fileContent)
+  def queryParam = resultJson.collect { it }.join('&')
+  return queryParam
 }
