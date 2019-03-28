a37ae0bea5bc25e56594b667e3f34df6974d4f29
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-28 14:03:08 -0600

    test

--- vars/lqlValidate.groovy
+++ vars/lqlValidate.groovy
@@ -1,13 +1,14 @@
-import groovy.json.JsonSlurperClassic
+import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 def call(String token, String testParam, String fileName) {
 
-    def fileContents = readFile fileName+".json"
-    print(fileContents)
-    final slurper = new JsonSlurperClassic()
-    Map queryParam = new HashMap<>(slurper.parseText(fileContents))
-    print(queryParam)
-
+    //def fileContents = readFile fileName+".json"
+    //print(fileContents)
+    //final slurper = new JsonSlurperClassic()
+    //Map queryParam = new HashMap<>(slurper.parseText(fileContents))
+    //print(queryParam)
+    def map = parseJson(fileName)
+    print(map)
     def curlCommand = [
         "curl --show-error --fail",
         "-XPOST 'https://api.lytics.io/api/query/_test?email=test@test.com&ucdmid=ucdmid"+testParam+"'",
@@ -17,4 +18,17 @@ def call(String token, String testParam, String fileName) {
         
     ]
     sh curlCommand.join(" ")
+}
+
+
+
+@NonCPS
+def parseJson(String fileName){
+  def fileContents = readFile fileName+".json"
+  def lazyMap = new groovy.json.JsonSlurper().parseText(fileContents)
+  def map = [:]
+  for ( prop in lazyMap ) {
+      map[prop.key] = prop.value
+  }
+  return map;
 }
