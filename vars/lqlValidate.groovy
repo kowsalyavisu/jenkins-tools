94f57cbd270aabd7d1632d0fdf3d73466de6d060
Kowsalya Viswanathan <kviswanathan@ancestry.com>
2019-03-28 13:59:37 -0600

    test

--- vars/lqlValidate.groovy
+++ vars/lqlValidate.groovy
@@ -2,7 +2,12 @@ import groovy.json.JsonSlurperClassic
 import groovy.json.JsonOutput
 def call(String token, String testParam, String fileName) {
 
-    def queryParam = parseJson(fileName)
+    def fileContents = readFile fileName+".json"
+    print(fileContents)
+    final slurper = new JsonSlurperClassic()
+    Map queryParam = new HashMap<>(slurper.parseText(fileContents))
+    print(queryParam)
+
     def curlCommand = [
         "curl --show-error --fail",
         "-XPOST 'https://api.lytics.io/api/query/_test?email=test@test.com&ucdmid=ucdmid"+testParam+"'",
@@ -12,15 +17,4 @@ def call(String token, String testParam, String fileName) {
         
     ]
     sh curlCommand.join(" ")
-}
-
-@NonCPS
-def parseJson(String fileName) {
-
-    def fileContents = readFile fileName+".json"
-    print(fileContents)
-    final slurper = new JsonSlurperClassic()
-    Map queryParam = new HashMap<>(slurper.parseText(fileContents))
-    print(queryParam)
-    return queryParam
 }
